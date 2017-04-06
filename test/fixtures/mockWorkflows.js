b80c3ea79f3bf452fb0b607ca37122d60b69244f
Jameel Briones <jbriones@localhost.localdomain>
2017-04-06 15:05:53 +0100

    Added sync error handler, editorconfig and debug logger

---
+++ test/fixtures/mockWorkflows.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: "id_01",
+    title: "Mock Workflow 1"
+  },
+  {
+    id: "id_02",
+    title: "Mock Workflow 2"
+  },
+  {
+    id: "id_03",
+    title: "Mock Workflow 3"
+  }
+];
